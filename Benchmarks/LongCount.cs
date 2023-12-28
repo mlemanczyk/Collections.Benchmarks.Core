a216c028a0e68136d8a668d1343f7f91261b300b
mlemanczyk <marek@lemanczyk.eu>
2023-12-28 19:47:02 +0100

    Benchmarks added & reorganized.

---
+++ Benchmarks/LongCount.cs
@@ -0,0 +1,19 @@
+﻿using Collections.Benchmarks.Core;
+
+namespace Recyclable.Collections.Benchmarks
+{
+	public partial class RecyclableCollectionsBenchmarks
+	{
+		public void Array_LongCount()
+		{
+			var data = TestObjects;
+			DoNothing.With(data.LongLength);
+		}
+
+		public void RecyclableLongList_LongCount()
+		{
+			var data = TestObjectsAsRecyclableLongList;
+			DoNothing.With(data.LongCount);
+		}
+	}
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Recyclable.Collections.Benchmarks
